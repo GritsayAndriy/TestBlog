d173c67e9133c3a74db57e94664b9a95a7d85c6f
Gritsay Andriy <gritsayandriy@gmail.com>
2020-09-07 16:32:27 +0300

    This is a test blog project.

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -2,14 +2,16 @@
 @section('content')
     <div class="row">
         <div class="col-md-8 blog-main">
-            <h3 class="pb-3 mb-4 font-italic border-bottom">
-                From the Firehose
-            </h3>
             <div class="blog-post">
                 <h2 class="blog-post-title">{{ $post->title }}</h2>
                 <p class="blog-post-meta">{{ $post->created_at }}</p>
+                <p class="blog-post-meta">{{ $post->description }}</p>
                 <p>{{ $post->content }}</p>
             </div>
         </div>
+    </div>
+    <div>
+        <a class="btn btn-primary" href="/updating_post{{$post->id}}">Update</a>
+        <a class="btn btn-danger" href="/destroy{{$post->id}}">Delete</a>
     </div>
 @endsection
